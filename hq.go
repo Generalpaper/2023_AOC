3399c4069a93a6fd5c058e8cc4d9c9792c1fcfbf
Generalpaper <119557325+Generalpaper@users.noreply.github.com>
2023-12-04 18:09:38 +0800

    Add files via upload

---
+++ hq.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	// "strings"
+	// "log"
+	// "example.com/greetings"
+)
+
+func main() {
+	fmt.Println("gaming")
+	// day1()
+	// day2()
+	day3() //did not do part 2
+	// day4()
+}
